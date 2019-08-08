b75a2064a30859c0b32c74eb40ddb42c82aed129
NMoore42 <nicholas.moore5@icloud.com>
2019-08-08 06:55:20 -0500

    Updates

---
+++ precache-manifest.bed5f64214eaf85258f14d34851029fc.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e06b4f67f3f2d6d28f76dc0bbea49b6",
+    "url": "/techdegree-project-12-frontend/index.html"
+  },
+  {
+    "revision": "5f659b6b408cc43b2657",
+    "url": "/techdegree-project-12-frontend/static/css/2.545747f9.chunk.css"
+  },
+  {
+    "revision": "4468c80cef6b6321471c",
+    "url": "/techdegree-project-12-frontend/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5f659b6b408cc43b2657",
+    "url": "/techdegree-project-12-frontend/static/js/2.d5ed149f.chunk.js"
+  },
+  {
+    "revision": "4468c80cef6b6321471c",
+    "url": "/techdegree-project-12-frontend/static/js/main.bdf8f253.chunk.js"
+  },
+  {
+    "revision": "481a3463877df4aebf40",
+    "url": "/techdegree-project-12-frontend/static/js/runtime~main.30698e62.js"
+  },
+  {
+    "revision": "1d726f22b9fe0c08e4abf2bd0971e4c1",
+    "url": "/techdegree-project-12-frontend/static/media/ada.1d726f22.svg"
+  },
+  {
+    "revision": "f092ba2f6d071af8b5681e50588ae6d5",
+    "url": "/techdegree-project-12-frontend/static/media/ada.f092ba2f.svg"
+  },
+  {
+    "revision": "0d7781f5295157e2009bec349f4c37ac",
+    "url": "/techdegree-project-12-frontend/static/media/bch.0d7781f5.svg"
+  },
+  {
+    "revision": "52f348b2794c6a3a651153db24a06ec7",
+    "url": "/techdegree-project-12-frontend/static/media/bch.52f348b2.svg"
+  },
+  {
+    "revision": "016e17971a5c621d8338c82652f738b2",
+    "url": "/techdegree-project-12-frontend/static/media/btc.016e1797.svg"
+  },
+  {
+    "revision": "e73cb1b222d746763a3d57ab92c94621",
+    "url": "/techdegree-project-12-frontend/static/media/btc.e73cb1b2.svg"
+  },
+  {
+    "revision": "0576375f768e1d70abf262063ecfb7fe",
+    "url": "/techdegree-project-12-frontend/static/media/eos.0576375f.svg"
+  },
+  {
+    "revision": "d431754f66fcced10457921c2ecd8627",
+    "url": "/techdegree-project-12-frontend/static/media/eos.d431754f.svg"
+  },
+  {
+    "revision": "18708d3e3f54116e2ea639fdd56dd6c5",
+    "url": "/techdegree-project-12-frontend/static/media/eth.18708d3e.svg"
+  },
+  {
+    "revision": "92b1ce207b1400a864b9c05e15cb5b0f",
+    "url": "/techdegree-project-12-frontend/static/media/eth.92b1ce20.svg"
+  },
+  {
+    "revision": "bf9c2c6e5d60d26d32a78c111dd0b1b4",
+    "url": "/techdegree-project-12-frontend/static/media/logo.bf9c2c6e.png"
+  },
+  {
+    "revision": "c1d013c90454abdd475f6d7f060f5af5",
+    "url": "/techdegree-project-12-frontend/static/media/ltc.c1d013c9.svg"
+  },
+  {
+    "revision": "cdae08509e949c7b6216a744b14f0087",
+    "url": "/techdegree-project-12-frontend/static/media/ltc.cdae0850.svg"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/techdegree-project-12-frontend/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "ab42a4bc9cd768301694bc005204efc1",
+    "url": "/techdegree-project-12-frontend/static/media/trx.ab42a4bc.svg"
+  },
+  {
+    "revision": "d974e8da282a42981f44125326436eab",
+    "url": "/techdegree-project-12-frontend/static/media/trx.d974e8da.svg"
+  },
+  {
+    "revision": "2cf84eeda99f1d0dd0299c99df7e453f",
+    "url": "/techdegree-project-12-frontend/static/media/xlm.2cf84eed.svg"
+  },
+  {
+    "revision": "da4b4503d6280d770ae7c8c3b4899996",
+    "url": "/techdegree-project-12-frontend/static/media/xlm.da4b4503.svg"
+  },
+  {
+    "revision": "97634f706aef8d220373572084dd4483",
+    "url": "/techdegree-project-12-frontend/static/media/xrp.97634f70.svg"
+  },
+  {
+    "revision": "d49b39860a73ea262bb796fbea4056b5",
+    "url": "/techdegree-project-12-frontend/static/media/xrp.d49b3986.svg"
+  },
+  {
+    "revision": "1224eecfe2bd7dd62c76ceeaf46b2ff8",
+    "url": "/techdegree-project-12-frontend/static/media/zec.1224eecf.svg"
+  },
+  {
+    "revision": "5a906a1de9cce8b5f4aaae80b26762cd",
+    "url": "/techdegree-project-12-frontend/static/media/zec.5a906a1d.svg"
+  }
+]);
